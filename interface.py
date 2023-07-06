354930128669f708e90c6bd7e68c48b53230e62c
username-nole <88860912+username-nole@users.noreply.github.com>
2023-07-06 10:25:30 -0400

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,7 +2,26 @@ import cv2
 from PyQt5.QtCore import Qt, QMimeData
 from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QImage, QPixmap, QFont
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget
-from usage import analysis
+from PyQt5 import uic
+# from usage import img_analysis
+from PIL import Image
+
+class MyGUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("form.ui", self)
+
+        #self.label.setPixmap(QPixmap('asset/sample_1.jpg'))
+        self.label.setText('fake')
+        self.label_2.setPixmap(QPixmap('asset/sample_1.jpg'))
+        self.label_3.setPixmap(QPixmap('pred_mask.png'))
+        self.label_4.setPixmap(QPixmap('result_tsne.png'))
+        self.label_5.setPixmap(QPixmap('result_feat_32.png'))
+        self.label_6.setPixmap(QPixmap('result_feat_64.png'))
+        self.label_7.setPixmap(QPixmap('result_feat_128.png'))
+        self.label_8.setPixmap(QPixmap('result_feat_256.png'))
+        self.show()
+
 
 class ImageWindow(QMainWindow):
     def __init__(self):
