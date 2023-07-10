992c080cf715c74f62260202eef351c27e942506
username-nole <88860912+username-nole@users.noreply.github.com>
2023-07-10 16:34:04 -0400

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,9 +1,15 @@
+# still in development
+# change the color scheme (top priority) - psuedo code commented out at the end of program.
+# add a sound when the interface starts (top priority)
+# add functionality so that multiple image files can be taken in the interface isntead of just 1????
+# add some sort of error handling?
 import cv2
-from PyQt5.QtCore import Qt, QMimeData
-from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QImage, QPixmap, QFont
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt, QMimeData, QPropertyAnimation, QPoint
+from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QImage, QPixmap, QFont, QScreen
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, \
+    QMessageBox
 from PyQt5 import uic
-from usage import img_analysis
+# from usage import img_analysis
 from PIL import Image
 
 class MyGUI(QMainWindow):
@@ -11,13 +17,13 @@ class MyGUI(QMainWindow):
         super().__init__()
         uic.loadUi("form.ui", self)
 
-        #self.label.setPixmap(QPixmap('asset/sample_1.jpg'))
         self.setWindowTitle("Image Viewer")
         self.setAcceptDrops(True)
 
-        self.label.setPixmap(QPixmap('asset/sample_1.jpg'))
-        self.label.setText('fake')
+        self.center_window()
+
         self.label_2.setPixmap(QPixmap('asset/sample_1.jpg'))
+        self.label.setText('fake')
         self.label_3.setPixmap(QPixmap('pred_mask.png'))
         self.label_4.setPixmap(QPixmap('result_tsne.png'))
         self.label_5.setPixmap(QPixmap('result_feat_32.png'))
@@ -26,20 +32,7 @@ class MyGUI(QMainWindow):
         self.label_8.setPixmap(QPixmap('result_feat_256.png'))
         self.show()
 
-    def update_images(self, image_path):
-        binary_mask = generate_binary_mask(image_path)
-        feature_map_1 = generate_feature_map_1(image_path)
-        feature_map_2 = generate_feature_map_2(image_path)
-        feature_map_3 = generate_feature_map_3(image_path)
-        feature_map_4 = generate_feature_map_4(image_path)
-        tsne_result = generate_tsne_result(image_path)
-
-        self.label_3.setPixmap(QPixmap.fromImage(binary_mask))
-        self.label_4.setPixmap(QPixmap.fromImage(tsne_result))
-        self.label_5.setPixmap(QPixmap.fromImage(feature_map_1))
-        self.label_6.setPixmap(QPixmap.fromImage(feature_map_2))
-        self.label_7.setPixmap(QPixmap.fromImage(feature_map_3))
-        self.label_8.setPixmap(QPixmap.fromImage(feature_map_4))
+        self.center_window()
 
 
 class ImageWindow(QMainWindow):
@@ -49,17 +42,26 @@ class ImageWindow(QMainWindow):
         self.setWindowTitle("Image Viewer")
         self.setAcceptDrops(True)
 
+        self.center_window()
+
         self.image_label = QLabel(self)
         self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.image_path = None
         self.setCentralWidget(self.image_label)
 
-        self.placeholder_text = "Drag and drop an image or click the button below to select"
-        self.image_label.setText(self.placeholder_text)
-        self.image_label.setFont(QFont("Arial", 12, weight=QFont.Weight.Light))
+        self.welcome_label = QLabel("Welcome, press the Space key to proceed", self)
+        self.welcome_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.welcome_label.setFont(QFont("Arial", 24, weight=QFont.Weight.Bold))
+        self.welcome_label.setGeometry(0, 0, self.width(), self.height())
+
+        self.animation = QPropertyAnimation(self.welcome_label, b"pos")
+        self.animation.setDuration(1500)
+        self.animation.setStartValue(QPoint(0, -self.height()))
+        self.animation.setEndValue(QPoint(0, 0))
 
         self.select_button = QPushButton("Select Image", self)
         self.select_button.clicked.connect(self.select_image)
+        self.select_button.setVisible(False)
 
         self.ok_button = QPushButton("OK", self)
         self.ok_button.clicked.connect(self.confirm_selection)
@@ -67,6 +69,7 @@ class ImageWindow(QMainWindow):
         self.ok_button.setVisible(False)
 
         layout = QVBoxLayout()
+        layout.addWidget(self.welcome_label)
         layout.addWidget(self.image_label)
         layout.addWidget(self.ok_button)
         layout.addWidget(self.select_button)
@@ -77,6 +80,34 @@ class ImageWindow(QMainWindow):
 
         self.selected_image_array = None
 
+        self.animation.start()
+
+        self.show_welcome_screen()
+
+    def center_window(self):
+        screen_geometry = QApplication.screens()[0].geometry()
+        window_geometry = self.geometry()
+        window_geometry.moveCenter(screen_geometry.center())
+        self.setGeometry(window_geometry)
+
+    def show_welcome_screen(self):
+        self.welcome_label.setVisible(True)
+        self.image_label.setVisible(False)
+        self.select_button.setVisible(False)
+        self.ok_button.setVisible(False)
+
+    def show_image_selection_screen(self):
+        self.welcome_label.setVisible(False)
+        self.image_label.setVisible(True)
+        self.select_button.setVisible(True)
+        self.ok_button.setVisible(True)
+
+        self.image_label.setText("Drag and drop an image or click the button below to select")
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.show_image_selection_screen()
+
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
             event.acceptProposedAction()
@@ -85,7 +116,6 @@ class ImageWindow(QMainWindow):
         if event.mimeData().hasUrls():
             url = event.mimeData().urls()[0]
             image_path = url.toLocalFile()
-            #self.image_path = image_path
             self.display_image(image_path)
 
     def select_image(self):
@@ -96,27 +126,23 @@ class ImageWindow(QMainWindow):
             selected_files = file_dialog.selectedFiles()
             if selected_files:
                 image_path = selected_files[0]
-                #self.image_path = image_path
                 self.display_image(image_path)
 
     def display_image(self, image_path: str):
         image = QImage(image_path)
         pixmap = QPixmap.fromImage(image)
 
-        maxWidth = 800
-        maxHeight = 600
+        maxWidth = 700
+        maxHeight = 500
 
         scaledPixmap = pixmap.scaled(maxWidth, maxHeight, Qt.AspectRatioMode.KeepAspectRatio)
 
         self.image_label.clear()
-
         self.image_label.setPixmap(scaledPixmap)
         self.image_label.setText("")
         self.image_label.adjustSize()
 
         self.resize(image.width(), image.height())
-        binary_mask = img_analysis(image_path)
-        binary_mask.save('pred_mask.png')
 
         image_array = cv2.imread(image_path)
         if image_array is not None:
@@ -124,7 +150,7 @@ class ImageWindow(QMainWindow):
             self.ok_button.setEnabled(True)
             self.ok_button.setVisible(True)
             print("Selected Image Array:")
-            #print(self.selected_image_array)
+            # print(self.selected_image_array)
             #binary_mask = analysis(self.selected_image_array)
             #binary_mask.save('pred_mask.png')
         else:
@@ -133,12 +159,6 @@ class ImageWindow(QMainWindow):
 
     def confirm_selection(self):
         self.secondW = MyGUI()
-        # if self.selected_image_array is not None:
-        #     #binary_mask = img_analysis(self.image_path)
-        #     #binary_mask.save('pred_mask.png')
-        #     self.clear_image()
-        self.secondW.show()
-        self.secondW.update_images(self.image_path)
 
     def clear_image(self):
         self.image_label.clear()
@@ -147,11 +167,38 @@ class ImageWindow(QMainWindow):
         self.ok_button.setVisible(False)
         self.selected_image_array = None
 
-        
 if __name__ == "__main__":
     app = QApplication([])
     window = ImageWindow()
     window.select_button.move(10, 10)
     window.ok_button.move(10, 50)
     window.show()
-    app.exec_()
+    app.exec()
+"""
+
+# Define your color scheme
+primary_color = QColor(100, 149, 237)  # Dodger Blue
+secondary_color = QColor(135, 206, 250)  # Sky Blue
+text_color = QColor(46, 49, 49)  # Dark Gray
+
+# Create a color palette
+palette = QApplication.palette()
+
+# Set colors for different roles
+palette.setColor(QPalette.Window, secondary_color)
+palette.setColor(QPalette.WindowText, text_color)
+palette.setColor(QPalette.Base, secondary_color)
+palette.setColor(QPalette.AlternateBase, primary_color)
+palette.setColor(QPalette.Button, primary_color)
+palette.setColor(QPalette.ButtonText, text_color)
+palette.setColor(QPalette.Text, text_color)
+palette.setColor(QPalette.Highlight, primary_color)
+palette.setColor(QPalette.HighlightedText, text_color)
+
+# Apply the color palette to the application
+QApplication.setPalette(palette)
+
+# Update the stylesheet to match the new colors
+app.setStyleSheet("QWidget { background-color: %s; }" % secondary_color.name())
+
+"""
