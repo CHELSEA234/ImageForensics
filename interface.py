57fa203bda619e729cbcda98f0423a22085d63f4
username-nole <88860912+username-nole@users.noreply.github.com>
2023-07-11 17:32:47 -0400

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,8 +1,19 @@
+
+# still in development
+# change the color scheme (top priority) - psuedo code commented out at the end of program.
+# add a sound when the interface starts (top priority)
+# add functionality so that multiple image files can be taken in the interface isntead of just 1????
+# add some sort of error handling?
 import cv2
-from PyQt5.QtCore import Qt, QMimeData, QPropertyAnimation, QPoint
-from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QImage, QPixmap, QFont
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt, QMimeData, QPropertyAnimation, QPoint, QTimer, QUrl
+from PyQt5.QtMultimedia import QSoundEffect
+from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QImage, QPixmap, QFont, QScreen
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, \
+    QMessageBox
+from PyQt5 import uic
+# from usage import img_analysis
 from PIL import Image
+
 
 class MyGUI(QMainWindow):
     def __init__(self):
@@ -42,12 +53,37 @@ class MyGUI(QMainWindow):
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+        """
+        super().__init__()
+        uic.loadUi("form.ui", self)
 
-    def center_window(self):
-        screen_geometry = QApplication.primaryScreen().geometry()
-        window_geometry = self.geometry()
-        window_geometry.moveCenter(screen_geometry.center())
-        self.setGeometry(window_geometry)
+        self.setWindowTitle("Image Viewer")
+        self.setAcceptDrops(True)
+
+        self.center_window()
+
+        self.label_2.setPixmap(QPixmap('asset/sample_1.jpg'))
+        self.label.setText('fake')
+        self.label_3.setPixmap(QPixmap('pred_mask.png'))
+        self.label_4.setPixmap(QPixmap('result_tsne.png'))
+        self.label_5.setPixmap(QPixmap('result_feat_32.png'))
+        self.label_6.setPixmap(QPixmap('result_feat_64.png'))
+        self.label_7.setPixmap(QPixmap('result_feat_128.png'))
+        self.label_8.setPixmap(QPixmap('result_feat_256.png'))
+        self.show()
+
+        self.center_window()
+
+
+        """
+        """
+          def center_window(self):
+            screen_geometry = QApplication.primaryScreen().geometry()
+            window_geometry = self.geometry()
+            window_geometry.moveCenter(screen_geometry.center())
+            self.setGeometry(window_geometry)
+
+        """
 
 
 class ImageWindow(QMainWindow):
@@ -64,12 +100,37 @@ class ImageWindow(QMainWindow):
         self.image_path = None
         self.setCentralWidget(self.image_label)
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_timeout)
+        stylesheet = """
+            QMainWindow {
+                background-color: black;
+            }
+
+            QLabel {
+                color: white;
+            }
+        """
+
+        self.setStyleSheet(stylesheet)
+
+        self.welcome_label = QLabel("Welcome!", self)
+        self.welcome_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.welcome_label.setFont(QFont("Arial", 24, weight=QFont.Weight.Bold))
+        self.welcome_label.setGeometry(0, 0, self.width(), self.height())
+
+        self.animation = QPropertyAnimation(self.welcome_label, b"pos")
+        self.animation.setDuration(1500)
+        self.animation.setStartValue(QPoint(0, -self.height()))
+        self.animation.setEndValue(QPoint(0, 0))
+
         self.select_button = QPushButton("Select Image", self)
         self.select_button.clicked.connect(self.select_image)
+        # self.select_button.setVisible(False)
         self.select_button.setStyleSheet(
             """
             QPushButton {
-                background-color: #2196F3;
+                background-color: #1E88E5;
                 color: white;
                 border: none;
                 padding: 10px 20px;
@@ -77,20 +138,20 @@ class ImageWindow(QMainWindow):
                 font-weight: bold;
                 border-radius: 5px;
             }
-            
+
             QPushButton:hover {
-                background-color: #1976D2;
+                background-color: #0D47A1;
             }
             """
         )
-        
+
         self.ok_button = QPushButton("OK", self)
         self.ok_button.clicked.connect(self.confirm_selection)
         self.ok_button.setEnabled(False)
         self.ok_button.setStyleSheet(
             """
             QPushButton {
-                background-color: #4CAF50;
+                background-color: #B71C1C;
                 color: white;
                 border: none;
                 padding: 10px 20px;
@@ -98,14 +159,17 @@ class ImageWindow(QMainWindow):
                 font-weight: bold;
                 border-radius: 5px;
             }
-            
+
             QPushButton:hover {
-                background-color: #45a049;
+                background-color: #8B0000;
             }
             """
         )
 
+        # self.ok_button.setVisible(False)
+
         layout = QVBoxLayout()
+        layout.addWidget(self.welcome_label)
         layout.addWidget(self.image_label)
         layout.addWidget(self.ok_button)
         layout.addWidget(self.select_button)
@@ -116,29 +180,40 @@ class ImageWindow(QMainWindow):
 
         self.selected_image_array = None
 
+        self.animation.start()
+
         self.show_welcome_screen()
+        self.play_init_sound()
+
+    def play_init_sound(self):
+        sound = QSoundEffect()
+        sound.setSource(QUrl.fromLocalFile("/Users/noel/Desktop/1103 N Oak Terr.wav"))
+        sound.play()
 
     def center_window(self):
-        screen_geometry = QApplication.primaryScreen().geometry()
+        screen_geometry = QApplication.screens()[0].geometry()
         window_geometry = self.geometry()
         window_geometry.moveCenter(screen_geometry.center())
         self.setGeometry(window_geometry)
 
     def show_welcome_screen(self):
+        # self.welcome_label.setVisible(True)
         self.image_label.setVisible(False)
         self.select_button.setVisible(False)
         self.ok_button.setVisible(False)
+        self.timer.start(2500)
+
+    def timer_timeout(self):
+        self.timer.stop()
+        self.show_image_selection_screen()
 
     def show_image_selection_screen(self):
+        self.welcome_label.setVisible(False)
         self.image_label.setVisible(True)
         self.select_button.setVisible(True)
         self.ok_button.setVisible(True)
 
         self.image_label.setText("Drag and drop an image or click the button below to select")
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self.show_image_selection_screen()
 
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
@@ -176,31 +251,32 @@ class ImageWindow(QMainWindow):
 
         self.resize(image.width(), image.height())
 
-        self.ok_button.setEnabled(True)
-        self.ok_button.setVisible(True)
+        image_array = cv2.imread(image_path)
+        if image_array is not None:
+            self.selected_image_array = image_array
+            self.ok_button.setEnabled(True)
+            self.ok_button.setVisible(True)
+            print("Selected Image Array:")
+            # print(self.selected_image_array)
+            # binary_mask = analysis(self.selected_image_array)
+            # binary_mask.save('pred_mask.png')
+        else:
+            print("Failed to read the image file.")
+        return self.selected_image_array
 
     def confirm_selection(self):
         self.secondW = MyGUI()
 
+    def clear_image(self):
+        self.image_label.clear()
+        self.image_label.setText(self.placeholder_text)
+        self.ok_button.setEnabled(False)
+        self.ok_button.setVisible(False)
+        self.selected_image_array = None
+
+
 if __name__ == "__main__":
     app = QApplication([])
-    
-    # stylesheet of the app
-    app.setStyleSheet(
-        """
-        QMainWindow {
-            background-color: #F5F5F5;
-        }
-        
-        QLabel {
-            background-color: white;
-            border: 1px solid #CCCCCC;
-            border-radius: 5px;
-            padding: 10px;
-        }
-        """
-    )
-    
     window = ImageWindow()
     window.select_button.move(10, 10)
     window.ok_button.move(10, 50)
