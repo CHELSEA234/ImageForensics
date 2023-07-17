88b963a327f40135854616de3be18ea45467e73c
username-nole <88860912+username-nole@users.noreply.github.com>
2023-07-17 16:42:06 -0400

    Update interface.py
    Sound has been taken out.

--- interface.py
+++ interface.py
@@ -1,11 +1,12 @@
-import os
-import sys
 import cv2
-from PyQt5.QtCore import Qt, QTimer, QRect, QPropertyAnimation, QPoint, QThread, pyqtSignal
+from PyQt5.QtCore import Qt, QMimeData, QPropertyAnimation, QPoint, QTimer
 from PyQt5.QtGui import QDragEnterEvent, QDropEvent, QImage, QPixmap, QFont
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget
-from pydub import AudioSegment
-from pydub.playback import play
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, \
+    QMessageBox
+from PyQt5 import uic
+# from usage import img_analysis
+from PIL import Image
+
 
 class MyGUI(QMainWindow):
     def __init__(self):
@@ -18,9 +19,6 @@ class MyGUI(QMainWindow):
 
         self.label_2 = QLabel(self)
         self.label_2.setPixmap(QPixmap('asset/sample_1.jpg'))
-        self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.label_2.setScaledContents(True)
-        self.label_2.setMinimumSize(400, 300)  # Adjust the size as needed
         self.label = QLabel('fake', self)
         self.label_3 = QLabel(self)
         self.label_3.setPixmap(QPixmap('pred_mask.png'))
@@ -48,27 +46,38 @@ class MyGUI(QMainWindow):
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
-
-    def center_window(self):
-        screen_geometry = QApplication.primaryScreen().geometry()
-        window_geometry = self.geometry()
-        window_geometry.moveCenter(screen_geometry.center())
-        self.setGeometry(window_geometry)
-
-class AudioPlaybackThread(QThread):
-    finished_playing = pyqtSignal()
-
-    def __init__(self, sound_file_path):
+        """
         super().__init__()
-        self.sound_file_path = sound_file_path
+        uic.loadUi("form.ui", self)
 
-    def run(self):
-        audio = AudioSegment.from_wav(self.sound_file_path)
-        play(audio)
-        self.finished_playing.emit()
+        self.setWindowTitle("Image Viewer")
+        self.setAcceptDrops(True)
 
-    def finish(self):
-        self.finished_playing.emit()
+        self.center_window()
+
+        self.label_2.setPixmap(QPixmap('asset/sample_1.jpg'))
+        self.label.setText('fake')
+        self.label_3.setPixmap(QPixmap('pred_mask.png'))
+        self.label_4.setPixmap(QPixmap('result_tsne.png'))
+        self.label_5.setPixmap(QPixmap('result_feat_32.png'))
+        self.label_6.setPixmap(QPixmap('result_feat_64.png'))
+        self.label_7.setPixmap(QPixmap('result_feat_128.png'))
+        self.label_8.setPixmap(QPixmap('result_feat_256.png'))
+        self.show()
+
+        self.center_window()
+
+
+        """
+        """
+          def center_window(self):
+            screen_geometry = QApplication.primaryScreen().geometry()
+            window_geometry = self.geometry()
+            window_geometry.moveCenter(screen_geometry.center())
+            self.setGeometry(window_geometry)
+
+        """
+
 
 class ImageWindow(QMainWindow):
     def __init__(self):
@@ -78,15 +87,6 @@ class ImageWindow(QMainWindow):
         self.setAcceptDrops(True)
 
         self.center_window()
-
-
-        # Get the directory path of the current script
-        script_directory = os.path.dirname(os.path.abspath(__file__))
-
-        # Construct the path to the sound file relative to the script directory
-        sound_file_path = os.path.join(script_directory, "/home/cvlab/Downloads/interfaceAudio.wav")
-
-        self.sound_file_path = sound_file_path
 
         self.image_label = QLabel(self)
         self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
@@ -119,6 +119,7 @@ class ImageWindow(QMainWindow):
 
         self.select_button = QPushButton("Select Image", self)
         self.select_button.clicked.connect(self.select_image)
+        # self.select_button.setVisible(False)
         self.select_button.setStyleSheet(
             """
             QPushButton {
@@ -130,6 +131,7 @@ class ImageWindow(QMainWindow):
                 font-weight: bold;
                 border-radius: 5px;
             }
+
             QPushButton:hover {
                 background-color: #0D47A1;
             }
@@ -157,6 +159,8 @@ class ImageWindow(QMainWindow):
             """
         )
 
+        # self.ok_button.setVisible(False)
+
         layout = QVBoxLayout()
         layout.addWidget(self.welcome_label)
         layout.addWidget(self.image_label)
@@ -169,52 +173,35 @@ class ImageWindow(QMainWindow):
 
         self.selected_image_array = None
 
-        #self.animation.finished.connect(self.play_init_sound)
-        self.start_audio_playback()
         self.animation.start()
+
         self.show_welcome_screen()
-
-    def show_welcome_screen(self):
-        self.image_label.setVisible(False)
-        self.select_button.setVisible(False)
-        self.ok_button.setVisible(False)
-        self.welcome_label.setVisible(True)
-        self.timer.stop()
-        self.animation.start()
-        #self.play_init_sound()
-
-    def start_audio_playback(self):
-        self.welcome_label.setVisible(True)
-        self.image_label.setVisible(True)
-        self.select_button.setVisible(True)
-        self.ok_button.setVisible(True)
-        self.audio_thread = AudioPlaybackThread(self.sound_file_path)
-        self.audio_thread.finished_playing.connect(self.show_image_selection_screen)
-        self.audio_thread.start()
-
-    def play_init_sound(self):
-        audio = AudioSegment.from_wav(self.sound_file_path)
-        play(audio)
-
-    def timer_timeout(self):
-        self.timer.stop()
-        self.image_label.setVisible(True)
-        self.select_button.setVisible(True)
-        self.ok_button.setVisible(True)
 
     def center_window(self):
         screen_geometry = QApplication.screens()[0].geometry()
-        window_geometry = QRect(screen_geometry.center().x() - 350, screen_geometry.center().y() - 250, 700, 500)
+        window_geometry = self.geometry()
+        window_geometry.moveCenter(screen_geometry.center())
         self.setGeometry(window_geometry)
+
+    def show_welcome_screen(self):
+        # self.welcome_label.setVisible(True)
+        self.image_label.setVisible(False)
+        self.select_button.setVisible(False)
+        self.ok_button.setVisible(False)
+        self.timer.start(2500)
+
+    def timer_timeout(self):
+        self.timer.stop()
+        self.show_image_selection_screen()
 
     def show_image_selection_screen(self):
         self.welcome_label.setVisible(False)
         self.image_label.setVisible(True)
         self.select_button.setVisible(True)
         self.ok_button.setVisible(True)
+
         self.image_label.setText("Drag and drop an image or click the button below to select")
-        self.timer.start(2000)
-        self.animation.start()
+
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
             event.acceptProposedAction()
@@ -229,7 +216,6 @@ class ImageWindow(QMainWindow):
         file_dialog = QFileDialog()
         file_dialog.setNameFilter("Image Files (*.png *.jpg *.jpeg)")
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
-        file_dialog.setOption(QFileDialog.Option.HideNameFilterDetails, True)  # Hide the filter details
         if file_dialog.exec() == QFileDialog.DialogCode.Accepted:
             selected_files = file_dialog.selectedFiles()
             if selected_files:
@@ -250,22 +236,23 @@ class ImageWindow(QMainWindow):
         self.image_label.setText("")
         self.image_label.adjustSize()
 
+        self.resize(image.width(), image.height())
+
         image_array = cv2.imread(image_path)
         if image_array is not None:
             self.selected_image_array = image_array
             self.ok_button.setEnabled(True)
             self.ok_button.setVisible(True)
             print("Selected Image Array:")
-            self.image_path = image_path
+            # print(self.selected_image_array)
+            # binary_mask = analysis(self.selected_image_array)
+            # binary_mask.save('pred_mask.png')
         else:
             print("Failed to read the image file.")
         return self.selected_image_array
 
     def confirm_selection(self):
         self.secondW = MyGUI()
-        self.secondW.show()
-        self.secondW.label_2.setPixmap(QPixmap(self.image_path))
-        self.hide()
 
     def clear_image(self):
         self.image_label.clear()
@@ -274,10 +261,12 @@ class ImageWindow(QMainWindow):
         self.ok_button.setVisible(False)
         self.selected_image_array = None
 
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QApplication([])
     window = ImageWindow()
     window.select_button.move(10, 10)
     window.ok_button.move(10, 50)
     window.show()
-    sys.exit(app.exec_())
+    app.exec()
+
